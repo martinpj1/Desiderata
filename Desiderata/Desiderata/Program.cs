9ef13cc74a8aa7f4503a5b59851510ff30180238
Philip Martin <martinpj1@outlook.com>
2016-03-18 16:59:50 -0400

    Pushing an empty project up

---
+++ Desiderata/Desiderata/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desiderata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
